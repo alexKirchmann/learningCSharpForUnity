a0df716bc13c48fb1532f9d693b182b98dbb8f41
alexKirchmann <tauva@ukr.net>
2020-10-15 15:37:08 +0300

    Script7-8 OnTrigger and OnCollision 2D

---
+++ Script7.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script7 : MonoBehaviour {
+    public GameObject obj;
+    private GameObject instObj;
+    [SerializeField] private float moveSpeed = 4f;
+     
+    void Start() {
+        instObj = Instantiate(obj, new Vector2(0, 0), Quaternion.identity);
+    }
+    
+    void Update() {
+        float yPos = Input.GetAxis("Vertical");
+        instObj.transform.Translate(Vector2.up * (moveSpeed * yPos * Time.deltaTime));
+    }
+}
---
+++ Script8.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script8 : MonoBehaviour
+{
+    private void OnCollisionEnter2D (Collision2D other) {
+        print(other.gameObject.name);
+    }
+    
+    private void OnCollisionExit2D (Collision2D other) {
+        print(other.gameObject.name);    
+        Destroy(other.gameObject);
+    }
+
+    private void OnTriggerEnter2D (Collider2D other) {
+        print(other.gameObject.name);    
+        Destroy(other.gameObject);
+    }
+}
