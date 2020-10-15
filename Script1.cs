c55edaa4e33589b074418f345e36dec8d0e312af
alexKirchmann <tauva@ukr.net>
2020-10-15 13:27:59 +0300

    Script 1 - Awaken, Start, Update

---
+++ Script1.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class Script1 : MonoBehaviour
+    {
+        public int i = 0;
+        private int n = 0;
+
+        void Awaken()
+        { 
+            Debug.Log("Awaken: " + n);
+        }
+
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+            //Debug.Log("Update: " + i);
+        }
+
+        void FixedUpdate()
+        {
+        
+        }
+    }
+}
