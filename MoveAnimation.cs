f5dd1caba5e91ff6f68b4f7709f2de5cb8860e90
alexKirchmann <tauva@ukr.net>
2020-10-24 11:48:16 +0300

    Anim - Easings

--- MoveAnimation.cs
+++ MoveAnimation.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
 
 public class MoveAnimation : MonoBehaviour {
     private Vector2 startPosition;
@@ -13,8 +15,30 @@ public class MoveAnimation : MonoBehaviour {
     }
 
     void Update() {
-        transform.position = Vector2.Lerp(transform.position, targetPosition, timeFromStart/3);
+        transform.position = Vector2.Lerp(transform.position, targetPosition, EasingSquared(timeFromStart/3));
         timeFromStart += Time.deltaTime;
     }
+
+    float EasingLinear(float x) {
+        return x;
+    }
+
+    float EasingSquared(float x) {
+        return x * x;
+    }
+
+    float EasingInverseSquared(float x) {
+        return 1 - (1 - x) * (1 - x);
+    }
+
+    float EasingSmoothedSquared(float x) {
+        return x < 0.5 ? x * x * 2 : (1 - (1 - x) * (1 - x) * 2);
+    }
+
+    float EasingCube(float x) {
+        return x * x * x;
+    }
+    
+    //easings.com
 }
 
