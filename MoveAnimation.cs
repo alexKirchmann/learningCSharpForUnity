51d920dc3e7bdd9ca06c6f0237bc3b5fe14d880a
alexKirchmann <tauva@ukr.net>
2020-10-24 11:39:12 +0300

    Anim - Lerp

--- MoveAnimation.cs
+++ MoveAnimation.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveAnimation : MonoBehaviour {
+    private Vector2 startPosition;
     private Vector2 targetPosition;
+    private float timeFromStart = 0;
     
     void Start() {
+        startPosition = transform.position;
         targetPosition = (Vector2) transform.position + Vector2.right * 10;
     }
 
     void Update() {
-        transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * 10);
+        transform.position = Vector2.Lerp(transform.position, targetPosition, timeFromStart/3);
+        timeFromStart += Time.deltaTime;
     }
 }
+
